fa2344d33e0e7fe2077f9b0827f191187a1a6be5
范丰平 <263869564@qq.com>
2018-08-04 22:31:11 +0800

    20180804-2230 初始化宙斯平台前端 by 范丰平

---
+++ fanfengping-zeus-web/src/menu/aside.js
@@ -0,0 +1,13 @@
+// 菜单 侧边栏
+export default [
+    { path: '/index', title: '首页', icon: 'home' },
+    {
+        title: '数据管理',
+        icon: 'database',
+        children: [
+            { path: '/page1', title: '数据库列表' },
+            { path: '/page2', title: '表结构比对' },
+            { path: '/page3', title: '数据字典' }
+        ]
+    }
+]
