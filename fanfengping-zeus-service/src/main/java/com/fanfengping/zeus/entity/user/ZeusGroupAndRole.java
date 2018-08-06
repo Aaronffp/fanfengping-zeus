5c312b5f7cb5afb3bea96b4d3759ea4b7765f03d
范丰平 <263869564@qq.com>
2018-08-06 23:17:38 +0800

    20180806-2317 提交分组角色实体类 by 范丰平

--- fanfengping-zeus-service/src/main/java/com/fanfengping/zeus/entity/user/ZeusGroupAndRole.java
+++ fanfengping-zeus-service/src/main/java/com/fanfengping/zeus/entity/user/ZeusGroupAndRole.java
@@ -3,8 +3,16 @@ package com.fanfengping.zeus.entity.user;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class ZeusGroupAndRole {
+    @Id
+    private String id;
+    private String group_id;
+    private String role_id;
+    private String operator;
+    private String ctime;
+    private String utime;
 }
