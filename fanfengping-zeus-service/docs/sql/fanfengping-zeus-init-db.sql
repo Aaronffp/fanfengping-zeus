f86c0019477df87506e45202436647f9a67982e3
范丰平 <263869564@qq.com>
2018-08-30 00:16:37 +0800

    20180830-0016 数据表名称变更 by 范丰平

--- fanfengping-zeus-service/docs/sql/fanfengping-zeus-init-db.sql
+++ fanfengping-zeus-service/docs/sql/fanfengping-zeus-init-db.sql
@@ -181,5 +181,34 @@ INSERT INTO shortcut(dis, disorder, name, icon, url, manager, mobile, operator, 
 
 
 
+-- ----------------------------
+-- Table structure for user(用户列表)
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
+(
+  `id` int PRIMARY KEY NOT NULL COMMENT '系统主键' AUTO_INCREMENT,
+  `uuid` varchar(50) NOT NULL COMMENT 'UUID',
+  `name` varchar(50) DEFAULT '规划用户' NOT NULL COMMENT '用户名称',
+  `mobile` varchar(15) DEFAULT '' NOT NULL COMMENT '手机号码',
+  `email` varchar(50) DEFAULT '' NOT NULL COMMENT '用户邮箱',
+  `account` varchar(50) DEFAULT '' NOT NULL COMMENT '用户账号（字母数字下划线）',
+  `passwd` varchar(50) DEFAULT '' NOT NULL COMMENT '用户密码',
+  `valid` int DEFAULT 0 NOT NULL COMMENT '是否启用（默认注销。1，启用；0，注销）',
+  `operator` varchar(50) DEFAULT 'system' NOT NULL COMMENT '操作人',
+  `ctime` datetime DEFAULT NOW() NOT NULL COMMENT '创建时间',
+  `utime` datetime DEFAULT now() NOT NULL COMMENT '更新时间',
+  UNIQUE INDEX `user_account_uindex` (`account`) COMMENT '用户表/用户账号 唯一键'
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户表';
+
+INSERT INTO user(uuid, name, mobile, email, account, passwd, valid, operator, ctime, utime) VALUES
+  (REPLACE(UUID(), '-', ''), '超级管理员', '15213149225', '263869564@qq.com', 'system', 'zeus123456', '1', '范丰平', NOW(), NOW()),
+  (REPLACE(UUID(), '-', ''), '管理员', '15213149225', '263869564@qq.com', 'admin', 'zeus123456', '1', '范丰平', NOW(), NOW()),
+  (REPLACE(UUID(), '-', ''), '普通用户', '15213149225', '263869564@qq.com', 'user', 'zeus123456', '1', '范丰平', NOW(), NOW()),
+  (REPLACE(UUID(), '-', ''), '访客', '15213149225', '263869564@qq.com', 'guest', 'zeus123456', '1', '范丰平', NOW(), NOW()),
+  (REPLACE(UUID(), '-', ''), '匿名用户', '15213149225', '263869564@qq.com', 'anon', 'zeus123456', '1', '范丰平', NOW(), NOW());
+
+
+
 
 
