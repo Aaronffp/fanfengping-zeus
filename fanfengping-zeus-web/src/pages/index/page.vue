2fde45799c548d7985d832f73cba111a565ab3af
范丰平 <263869564@qq.com>
2018-09-04 02:13:15 +0800

    20180904-0213 修改首页显示内容；更改github链接 by 范丰平

--- fanfengping-zeus-web/src/pages/index/page.vue
+++ fanfengping-zeus-web/src/pages/index/page.vue
@@ -1,20 +1,19 @@
 <template>
   <d2-container class="page">
     <d2-page-cover
-      title="I AM D2ADMIN"
+      title="I AM ZEUSADMIN"
       sub-title="追求简约美感的后台管理系统集成方案">
       <d2-icon-svg style="width: 120px;" name="d2-admin"/>
       <template slot="footer">
         <div class="page__btn-group">
-          <span @click="$open('https://github.com/d2-projects')">项目组</span> |
-          <span @click="$open('http://app.d3collection.cn/d2-admin-doc/lastest/zh/')">使用文档</span> |
-          <span @click="$open('https://github.com/d2-projects/d2-admin-start-kit')">简化版脚手架</span> |
-          <span @click="$open('https://alibaba.github.io/ice/scaffold?type=vue')">飞冰物料</span> |
-          <span @click="$open('https://juejin.im/user/57a48b632e958a006691b946/posts')">掘金专栏</span> |
+          <span @click="$open('https://github.com/Aaronffp/fanfengping-zeus.git')">GitHub</span> |
+          <span @click="$open('https://github.com/Aaronffp/fanfengping-zeus.git')">使用文档</span> |
+          <span @click="$open('http://www.fanfengping.com/')">个人网站</span> |
+          <span @click="$open('http://www.cnblogs.com/fengpingfan/')">个人博客</span> |
           <el-popover
             :width="172"
             trigger="hover">
-            <p class="d2-mt-0 d2-mb-10">D2Projects</p>
+            <p class="d2-mt-0 d2-mb-10">ZEUSProjects</p>
             <img src="./image/qr@2x.png" style="width: 172px;">
             <span slot="reference">微信公众号</span>
             <p
@@ -34,12 +33,12 @@
 </template>
 
 <script>
-import D2HelpBtn from './components/d2-help-btn'
-export default {
-  components: {
-    D2HelpBtn
-  }
-}
+// import D2HelpBtn from './components/d2-help-btn'
+// export default {
+//   components: {
+//     D2HelpBtn
+//   }
+// }
 </script>
 
 <style lang="scss" scoped>
