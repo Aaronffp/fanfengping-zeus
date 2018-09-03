ef45ae8122aa08d26a3ceb9682b91b4ef327309d
范丰平 <263869564@qq.com>
2018-09-04 01:38:40 +0800

    20180904-0138 初始设置加载侧边栏（main.js） by 范丰平

--- fanfengping-zeus-web/src/main.js
+++ fanfengping-zeus-web/src/main.js
@@ -53,6 +53,8 @@ new Vue({
     this.$store.commit('d2admin/page/init', frameInRoutes)
     // 设置顶栏菜单
     this.$store.commit('d2admin/menu/headerSet', menuHeader)
+    // 设置侧边栏菜单
+    this.$store.commit('d2admin/menu/asideSet', menuAside)
     // 初始化菜单搜索功能
     this.$store.commit('d2admin/search/init', menuHeader)
   },
