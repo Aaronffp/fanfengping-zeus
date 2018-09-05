1f9aa4faaa6bc47199b880f6cf88b07a18f85e63
范丰平 <263869564@qq.com>
2018-09-06 01:42:45 +0800

    20180906-0142 修改eslint规则 by 范丰平

--- fanfengping-zeus-web/.eslintrc.js
+++ fanfengping-zeus-web/.eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    'indent': [2, 4],
+    'space-before-function-paren': [0, 'always'],    //函数定义时括号前面要不要有空格
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
