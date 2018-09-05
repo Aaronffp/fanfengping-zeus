6a65a9cd28668e3a8ff4c4fab0bb4e8a400f8d00
范丰平 <263869564@qq.com>
2018-09-06 01:43:20 +0800

    20180906-0143 修改AXIOS by 范丰平

--- fanfengping-zeus-web/src/plugin/axios/index.js
+++ fanfengping-zeus-web/src/plugin/axios/index.js
@@ -2,11 +2,15 @@ import store from '@/store'
 import axios from 'axios'
 import { Message } from 'element-ui'
 import util from '@/libs/util'
+import router from '../../router'
+
+// 使用vuex做全局loading时使用
+// import store from '@/store'
 
 // 创建一个错误
 function errorCreat (msg) {
-  const err = new Error(msg)
-  errorLog(err)
+  const err = new Error(msg);
+  errorLog(err);
   throw err
 }
 
@@ -17,10 +21,10 @@ function errorLog (err) {
     type: 'error',
     err,
     info: '数据请求异常'
-  })
+  });
   // 打印到控制台
   if (process.env.NODE_ENV === 'development') {
-    util.log.danger('>>>>>> Error >>>>>>')
+    util.log.danger('>>>>>> Error <<<<<<');
     console.log(err)
   }
   // 显示提示
@@ -31,80 +35,115 @@ function errorLog (err) {
   })
 }
 
-// 创建一个 axios 实例
+// 创建 AXIOS 实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_API,
-  timeout: 5000 // 请求超时时间
-})
+  headers: {
+    'Content-Type': 'application/json;charset=utf8'
+  },
+  withCredentials: true,
+  timeout: 3000,
+  // 响应数据类型
+  responseType: 'json',
+  // 响应编码
+  responseEncoding: 'utf8'
+});
 
 // 请求拦截器
 service.interceptors.request.use(
   config => {
     // 在请求发送之前做一些处理
+    // 1. 请求开始的时候可以结合 vuex 开启全屏 loading 动画
+    // console.log(store.state.loading)
+    // console.log('准备发送请求...')
+    
     if (!(/^https:\/\/|http:\/\//.test(config.url))) {
-      const token = util.cookies.get('token')
+      const token = util.cookies.get('token');
       if (token && token !== 'undefined') {
         // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
-        config.headers['X-Token'] = token
+        config.headers['token'] = token
       }
     }
     return config
   },
+  
   error => {
-    // 发送失败
-    console.log(error)
+    const errorUrl = error.url;
+    const errorInfo = error.response;
+    
+    // 请求发送失败
+    console.log('Request: ' + errorUrl, error);
+    console.log(errorInfo);
+    
+    // 判断是否请求超时
+    if (error.code === 'ECONNABORTED' && error.message.indexOf('timeout') !== -1) {
+      errorCreat(`[ Request Timeout ] ${errorUrl} : ${errorInfo}`);
+      console.log('Request Timeout', errorInfo);
+    }
+    
+    // 错误页面重定向
+    if (errorInfo) {
+      const errorStatus = errorInfo.status; // 404 403 500 ...
+      router.push({
+        path: `/error/${errorStatus}`
+      })
+    }
     Promise.reject(error)
   }
-)
+);
 
 // 响应拦截器
 service.interceptors.response.use(
   response => {
     // dataAxios 是 axios 返回数据中的 data
-    const dataAxios = response.data
-    // 这个状态码是和后端约定的
-    const { code } = dataAxios
-    // 根据 code 进行判断
+    let dataAxios;
+    // 浏览器IE9，响应为undefined获取相应信息
+    if (response.data === undefined) {
+      dataAxios = JSON.parse(response.request.responseText)
+    } else {
+      dataAxios = response.data
+    }
+    
+    // 后端业务服务状态码
+    const { code } = dataAxios;
+    // 根据 code 进行先期响应码处理
     if (code === undefined) {
       // 如果没有 code 代表这不是项目后端开发的接口 比如可能是 D2Admin 请求最新版本
       return dataAxios
     } else {
       // 有 code 代表这是一个后端接口 可以进行进一步的判断
       switch (code) {
-        case 200:
-          // [ 示例 ] code === 0 代表没有错误
-          return dataAxios.data
-        case 'xxx':
-          // [ 示例 ] 其它和后台约定的 code
-          errorCreat(`[ code: xxx ] ${dataAxios.msg}: ${response.config.url}`)
-          break
-        default:
-          // 不是正确的 code
-          errorCreat(`${dataAxios.msg}: ${response.config.url}`)
-          break
+        case 200:    // 200 代表没有错误
+          return dataAxios.data;
+        case '999':  // 999 系统未知错误响应码
+          errorCreat(`[ code: 999 ] ${dataAxios.msg}: ${response.config.url}`);
+          break;
+        default:     // 其他未定义的响应码
+          errorCreat(`[ code: ${code} ] ${dataAxios.msg}: ${response.config.url}`);
+          break;
       }
     }
   },
   error => {
     if (error && error.response) {
       switch (error.response.status) {
-        case 400: error.message = '请求错误'; break
-        case 401: error.message = '未授权，请登录'; break
-        case 403: error.message = '拒绝访问'; break
-        case 404: error.message = `请求地址出错: ${error.response.config.url}`; break
-        case 408: error.message = '请求超时'; break
-        case 500: error.message = '服务器内部错误'; break
-        case 501: error.message = '服务未实现'; break
-        case 502: error.message = '网关错误'; break
-        case 503: error.message = '服务不可用'; break
-        case 504: error.message = '网关超时'; break
-        case 505: error.message = 'HTTP版本不受支持'; break
+        case 400: error.message = '请求错误'; break;
+        case 401: error.message = '未授权，请登录'; break;
+        case 403: error.message = '拒绝访问'; break;
+        case 404: error.message = `请求地址出错: ${error.response.config.url}`; break;
+        case 408: error.message = '请求超时'; break;
+        case 500: error.message = '服务器内部错误'; break;
+        case 501: error.message = '服务未实现'; break;
+        case 502: error.message = '网关错误'; break;
+        case 503: error.message = '服务不可用'; break;
+        case 504: error.message = '网关超时'; break;
+        case 505: error.message = 'HTTP版本不受支持'; break;
         default: break
       }
     }
-    errorLog(error)
+    errorLog(error);
     return Promise.reject(error)
   }
-)
+);
 
 export default service
