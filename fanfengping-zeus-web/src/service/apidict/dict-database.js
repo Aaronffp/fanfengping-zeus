8c88e9e4c997a7e53e0b31243961457b276cc079
范丰平 <263869564@qq.com>
2018-09-06 01:55:48 +0800

    20180906-0155 提交数据库接口信息定义 by 范丰平

---
+++ fanfengping-zeus-web/src/service/apidict/dict-database.js
@@ -0,0 +1,27 @@
+/**
+ * 接口信息定义
+ */
+
+export const API_DICT_DATABASE = {
+  URL_BASE: '/db',
+  URL_BENCHMARK: '/db/benchmark',
+  DATABASE: {
+    id: 0,
+    env: '',
+    eng: '',
+    chs: '',
+    benchmark: 0,
+    type: '',
+    driver: '',
+    url: '',
+    account: '',
+    password: '',
+    creater: '',
+    ctime: '',
+    updater: '',
+    utime: '',
+    note: ''
+  }
+};
+
+export default API_DICT_DATABASE
