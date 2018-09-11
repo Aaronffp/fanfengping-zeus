b7acc52e12c75455b3ff98944831bc7567dd67a4
范丰平 <263869564@qq.com>
2018-09-11 19:55:32 +0800

    20180911-1955 update none

--- fanfengping-zeus-web/src/pages/database/index/index.vue
+++ fanfengping-zeus-web/src/pages/database/index/index.vue
@@ -1,127 +1,124 @@
 <template>
-  <el-container>
-    <br>
-    <el-header>
-      <el-row>
-        <el-form :model="query" :inline="true">
-          <el-form-item>
-            <el-select v-model="query.env" clearable filterable placeholder="请选择数据库环境...">
-              <el-option v-for="item in conditions.envs" :key="item.value" :label="item.label" :value="item.value">
-                <span style="float: left">{{ item.label }}</span>
-                <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
-              </el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item>
-            <el-select v-model="query.eng" placeholder="请输入英文简称..." clearable>
-              <el-option v-for="item in conditions.engs" :key="item.value" :label="item.label" :value="item.value">
-                <span style="float: left">{{ item.label }}</span>
-                <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
-              </el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item>
-            <el-input v-model="query.chs" placeholder="请输入中文简称..." clearable></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button @click="handleBtnQuery(query)" type="primary" icon="el-icon-search">搜索</el-button>
-          </el-form-item>
-          <el-form-item>
-            <el-button @click="dialogFormVisible = true" type="primary" icon="el-icon-plus">新增</el-button>
-          </el-form-item>
-        </el-form>
-      </el-row>
+  <d2-container>
+    <el-form :model="query" :inline="true">
+      <el-form-item label="所属环境：">
+        <el-select v-model="query.env" clearable filterable placeholder="请选择数据库环境...">
+          <el-option v-for="item in conditions.envs" :key="item.value" :label="item.label" :value="item.value">
+            <span style="float: left">{{ item.label }}</span>
+            <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="英文简称：">
+        <el-select v-model="query.eng" placeholder="请输入英文简称..." clearable>
+          <el-option v-for="item in conditions.engs" :key="item.value" :label="item.label" :value="item.value">
+            <span style="float: left">{{ item.label }}</span>
+            <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="中文简称：">
+        <el-input v-model="query.chs" placeholder="请输入中文简称..." clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="handleBtnQuery(query)" type="primary" icon="el-icon-search">搜索</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="dialogFormVisible = true" type="primary" icon="el-icon-plus">新增</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="dialogFormVisible = true" type="primary" icon="el-icon-plus">导出</el-button>
+      </el-form-item>
+    </el-form>
     
-    </el-header>
-    <el-main>
-      <el-table
-        :data="tableData"
-        hight="250"
-        border
-        stripe
-        max-height="650"
-        style="width: 100%">
-        <el-table-column fixed type="index" width="50"></el-table-column>
-        <!--<el-table-column fixed prop="id" label="主键" width="50" v-show="false"></el-table-column>-->
-        <el-table-column fixed prop="eng" label="英文简称" width="180"></el-table-column>
-        <el-table-column prop="chs" label="中文简称" width="150"></el-table-column>
-        <el-table-column prop="env" label="环境标识" width="100"></el-table-column>
-        <el-table-column prop="benchmark" label="基准库" width="70"></el-table-column>
-        <el-table-column prop="type" label="数据库类型" width="100"></el-table-column>
-        <el-table-column prop="driver" label="数据库驱动" width="210"></el-table-column>
-        <el-table-column prop="url" label="数据库URL" width="400"></el-table-column>
-        <el-table-column prop="account" label="数据库账号" width="150"></el-table-column>
-        <el-table-column prop="password" label="数据库密码" width="150"></el-table-column>
-        <el-table-column prop="note" label="备注" width="200"></el-table-column>
-        <el-table-column prop="creater" label="创建人" width="100"></el-table-column>
-        <el-table-column prop="ctime" label="创建时间" width="170"></el-table-column>
-        <el-table-column prop="updater" label="更新人" width="100"></el-table-column>
-        <el-table-column prop="utime" label="更新时间" width="170"></el-table-column>
-        <el-table-column fixed="right" label="操作" width="100">
-          <template slot-scope="scope">
-            <el-button type="text" size="small">编辑</el-button>
-            <el-button type="text" size="small">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <el-dialog title="新增数据库" :visible.sync="dialogFormVisible">
-        <el-form :model="formData">
-          <el-form-item label="环境：" :label-width="formLabelWidth">
-            <el-select v-model="formData.env" clearable placeholder="请选择数据库环境...">
-              <el-option v-for="item in conditions.envs" :key="item.value" :label="item.label" :value="item.value">
-                <span style="float: left">{{ item.label }}</span>
-                <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
-              </el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="英文名称：" :label-width="formLabelWidth">
-            <el-input v-model="formData.eng" clearable placeholder="请输入英文名称..."></el-input>
-          </el-form-item>
-          <el-form-item label="中文名称：" :label-width="formLabelWidth">
-            <el-input v-model="formData.chs" clearable placeholder="请输入中文名称..."></el-input>
-          </el-form-item>
-          <el-form-item label="基准库：" :label-width="formLabelWidth">
-            <el-select v-model="formData.benchmark" placeholder="请选择...">
-              <el-option label="是" value="1"></el-option>
-              <el-option label="否" value="0"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="类型：" :label-width="formLabelWidth">
-            <el-select v-model="formData.type" placeholder="请选择数据库类型...">
-              <el-option label="MYSQL" value="MYSQL"></el-option>
-              <el-option label="ORACLE" value="ORACLE"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="驱动：" :label-width="formLabelWidth">
-            <el-select v-model="formData.driver" placeholder="请选择数据库驱动...">
-              <el-option label="MYSQL驱动" value="com.mysql.jdbc.Driver"></el-option>
-              <el-option label="ORACLE驱动" value="oracle.jdbc.driver.OracleDriver"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="链接：" :label-width="formLabelWidth">
-            <el-input v-model="formData.url" clearable placeholder="请输入链接...">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="账号：" :label-width="formLabelWidth">
-            <el-input v-model="formData.account" clearable placeholder="请输入账号...">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="密码：" :label-width="formLabelWidth">
-            <el-input v-model="formData.password" clearable placeholder="请输入密码...">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="备注：" :label-width="formLabelWidth">
-            <el-input v-model="formData.note" type="textarea" :autosize="{maxRows: 4}" clearable placeholder="请输入备注信息...">
-            </el-input>
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-        </div>
-      </el-dialog>
-    </el-main>
-  </el-container>
+    <el-table
+      :data="tableData"
+      hight="250"
+      border
+      stripe
+      show-overflow-tooltip
+      max-height="700"
+      style="width: 100%">
+      <el-table-column fixed type="index" width="50"></el-table-column>
+      <!--<el-table-column fixed prop="id" label="主键" width="50" v-show="false"></el-table-column>-->
+      <el-table-column fixed prop="eng" label="英文简称" width="160"></el-table-column>
+      <el-table-column prop="chs" label="中文简称" width="150"></el-table-column>
+      <el-table-column prop="env" label="环境标识" width="80"></el-table-column>
+      <el-table-column prop="benchmark" label="基准库" width="70"></el-table-column>
+      <el-table-column prop="type" label="数据库类型" width="100"></el-table-column>
+      <el-table-column prop="driver" label="数据库驱动" width="210"></el-table-column>
+      <el-table-column prop="url" label="数据库URL" width="400"></el-table-column>
+      <el-table-column prop="account" label="数据库账号" width="100"></el-table-column>
+      <el-table-column prop="password" label="数据库密码" width="100"></el-table-column>
+      <el-table-column prop="note" label="备注" width="200"></el-table-column>
+      <el-table-column prop="creater" label="创建人" width="80"></el-table-column>
+      <el-table-column prop="ctime" label="创建时间" width="170"></el-table-column>
+      <el-table-column prop="updater" label="更新人" width="80"></el-table-column>
+      <el-table-column prop="utime" label="更新时间" width="170"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button type="text" size="small">编辑</el-button>
+          <el-button type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog title="新增数据库" :visible.sync="dialogFormVisible">
+      <el-form :model="formData">
+        <el-form-item label="环境：" :label-width="formLabelWidth">
+          <el-select v-model="formData.env" clearable placeholder="请选择数据库环境...">
+            <el-option v-for="item in conditions.envs" :key="item.value" :label="item.label" :value="item.value">
+              <span style="float: left">{{ item.label }}</span>
+              <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span>
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="英文名称：" :label-width="formLabelWidth">
+          <el-input v-model="formData.eng" clearable placeholder="请输入英文名称..."></el-input>
+        </el-form-item>
+        <el-form-item label="中文名称：" :label-width="formLabelWidth">
+          <el-input v-model="formData.chs" clearable placeholder="请输入中文名称..."></el-input>
+        </el-form-item>
+        <el-form-item label="基准库：" :label-width="formLabelWidth">
+          <el-select v-model="formData.benchmark" placeholder="请选择...">
+            <el-option label="是" value="1"></el-option>
+            <el-option label="否" value="0"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="类型：" :label-width="formLabelWidth">
+          <el-select v-model="formData.type" placeholder="请选择数据库类型...">
+            <el-option label="MYSQL" value="MYSQL"></el-option>
+            <el-option label="ORACLE" value="ORACLE"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="驱动：" :label-width="formLabelWidth">
+          <el-select v-model="formData.driver" placeholder="请选择数据库驱动...">
+            <el-option label="MYSQL驱动" value="com.mysql.jdbc.Driver"></el-option>
+            <el-option label="ORACLE驱动" value="oracle.jdbc.driver.OracleDriver"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="链接：" :label-width="formLabelWidth">
+          <el-input v-model="formData.url" clearable placeholder="请输入链接...">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="账号：" :label-width="formLabelWidth">
+          <el-input v-model="formData.account" clearable placeholder="请输入账号...">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="密码：" :label-width="formLabelWidth">
+          <el-input v-model="formData.password" clearable placeholder="请输入密码...">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="备注：" :label-width="formLabelWidth">
+          <el-input v-model="formData.note" type="textarea" :autosize="{maxRows: 4}" clearable placeholder="请输入备注信息...">
+          </el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+  </d2-container>
 </template>
 
 <script>
