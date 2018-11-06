d0a2d7ae70d9dfb177b8d63cb2b5445267390288
范丰平 <263869564@qq.com>
2018-11-06 22:38:45 +0800

    20181105-2238 修改菜单项 by 范丰平

--- fanfengping-zeus-web/src/menu/modules/menu-project-manage.js
+++ fanfengping-zeus-web/src/menu/modules/menu-project-manage.js
@@ -3,7 +3,6 @@ export default {
   title: '项目管理',
   icon: 'flag',
   children: (pre => [
-    {path: `${pre}appr`, title: '项目立项', icon: 'flag-o'},
-    {path: `${pre}comp`, title: '数据库比对', icon: 'compress'}
+    {path: `${pre}appr`, title: '项目立项', icon: 'flag-o'}
   ])('/proj/')
 }
